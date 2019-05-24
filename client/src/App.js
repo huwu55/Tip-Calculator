e174d89556e3b6997afa37588c485a0535dd24c9
Huiling Wu <huwu@ucdavis.edu>
2019-05-24 13:11:04 -0700

    added calculate total per person method

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,16 @@ class App extends React.Component {
             bill : 0.00,
             tipPercentage : 0,
             tip : 0,
+            total: 0,
             num_of_ppl : 0,
             tip_per_person : 0,
+            total_per_person: 0,
             isSplitTip : false
         };
+    }
+
+    componentDidMount(){
+        this.setState({bill: (0).toFixed(2)});
     }
 
     // validate each time user enters a key
@@ -24,11 +30,12 @@ class App extends React.Component {
         let bill = parseInt(event.target.value.replace(/[^\d]/, ''));
 
         if(!bill)    {
-            bill = 0;
+            bill = (0).toFixed(2);
         }
         else{
             bill = (bill / 100).toFixed(2);
         }
+        //console.log(typeof bill, bill);
 
         //console.log("bill: ", bill);
         this.setState({bill}, () => this.calcTip());
@@ -48,13 +55,17 @@ class App extends React.Component {
     }
 
     calcTip = () => {
+        //console.log(typeof this.state.bill, typeof this.state.tipPercentage);
         let tip = (this.state.bill * this.state.tipPercentage * 0.01).toFixed(2);
-        this.setState({tip}, () => {
+        let total = (parseFloat(tip) + parseFloat(this.state.bill)).toFixed(2);
+        //console.log(typeof this.state.tip, typeof this.state.num_of_ppl);
+        this.setState({tip, total}, () => {
             if(this.state.isSplitTip && this.state.num_of_ppl !== 0){
                 // console.log("tip:", this.state.tip);
                 // console.log("numofppl:", this.state.num_of_ppl);
-                let tip_per_person = (this.state.tip / this.state.num_of_ppl).toFixed(2);
-                this.setState({tip_per_person});
+                let tip_per_person = (parseFloat(this.state.tip) / this.state.num_of_ppl).toFixed(2);
+                let total_per_person = (total / this.state.num_of_ppl).toFixed(2);
+                this.setState({tip_per_person, total_per_person});
             }
         });
     }
@@ -70,13 +81,16 @@ class App extends React.Component {
 
         let num_of_ppl = parseInt(event.target.value.replace(/[^\d]/, ''));
 
-        console.log("num_of_ppl:", num_of_ppl);
+        //console.log("num_of_ppl:", num_of_ppl);
 
         if(num_of_ppl && num_of_ppl !== 0){
-            let tip_per_person = (this.state.tip / num_of_ppl).toFixed(2);
-            this.setState({num_of_ppl, tip_per_person});
+            //console.log(typeof this.state.total);
+            let tip_per_person = (parseFloat(this.state.tip) / num_of_ppl).toFixed(2);
+            let total_per_person = (parseFloat(this.state.total) / num_of_ppl).toFixed(2);
+            //console.log(typeof this.state.num_of_ppl, this.state.num_of_ppl);
+            this.setState({num_of_ppl, tip_per_person, total_per_person});
         }
-        else this.setState({num_of_ppl: 0, tip_per_person : 0});
+        else this.setState({num_of_ppl: 0, tip_per_person : 0, total_per_person: 0});
     }
 
     render(){
@@ -95,8 +109,10 @@ class App extends React.Component {
                 <br/>
                 <ResultSection 
                     tip = {this.state.tip}
+                    total = {this.state.total}
                     isSplitTip = {this.state.isSplitTip}
                     tip_per_person = {this.state.tip_per_person}
+                    total_per_person = {this.state.total_per_person}
                 />
             </div>
         );
--- client/src/components/ResultSection.js
+++ client/src/components/ResultSection.js
@@ -5,9 +5,13 @@ export default (props) => {
         <div>
             Total Tip: {props.tip}
             <br/><br/>
+            Total Amount: {props.total}
+            <br/><br/>
             { props.isSplitTip && (
                 <div>
                     Tip Per Person: {props.tip_per_person}
+                    <br/><br/>
+                    Total Per Person: {props.total_per_person}
                 </div>
             )}
         </div>
