070b163648b7f41439bcdeab5df79f2a50cf47b8
Huiling Wu <huwu@ucdavis.edu>
2019-06-10 16:33:01 -0700

    Updates

---
+++ precache-manifest.adfeb4684ff1902d8f0b8733d5ad24e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba31b7945534af4d75120e5ac861c008",
+    "url": "/Tip-Calculator/index.html"
+  },
+  {
+    "revision": "85409990a0a77c80da78",
+    "url": "/Tip-Calculator/static/css/main.e633139a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Tip-Calculator/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "85409990a0a77c80da78",
+    "url": "/Tip-Calculator/static/js/main.cc24392c.chunk.js"
+  },
+  {
+    "revision": "15f2e1329546f1d39e18",
+    "url": "/Tip-Calculator/static/js/runtime~main.e2c2edbe.js"
+  }
+]);
